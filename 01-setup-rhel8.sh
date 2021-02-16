47f564337bf7cdb9775bedb81e7defe5196f8d75
rlucente <rlucente@redhat.com>
2021-02-16 12:00:50 -0500

    register with RHSM

---
+++ 01-setup-rhel8.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/demo.conf
+
+if [[ $EUID -ne 0 ]]
+then
+    echo
+    echo "*** MUST RUN AS root ***"
+    echo
+    exit 1
+fi
+
+subscription-manager register \
+    --username $USERNAME --password $PASSWORD || exit 1
+subscription-manager role --set="Red Hat Enterprise Linux Server"
+subscription-manager service-level --set="Self-Support"
+subscription-manager usage --set="Development/Test"
+subscription-manager attach
+
+yum -y update
+yum -y clean all
+
