accc9d50da8dbcc7a0b325e43102b39e647c69b7
rlucente <rlucente@redhat.com>
2021-02-16 12:01:27 -0500

    adjust edge.ks with user parms

---
+++ 02-config-image-builder.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+. $(dirname $0)/demo.conf
+
+if [[ $EUID -ne 0 ]]
+then
+    echo
+    echo "*** MUST RUN AS root ***"
+    echo
+    exit 1
+fi
+
+# install image builder and other necessary packages
+yum -y install osbuild-composer composer-cli cockpit-composer \
+    bash-completion jq
+
+# enable image builder to start after reboot
+systemctl enable --now cockpit.socket osbuild-composer.socket
+
+# add user to weldr group so they don't need to be root to run image builder
+[[ ! -z "$SUDO_USER" ]] && usermod -aG weldr $SUDO_USER
+
+firewall-cmd --permanent --add-port=8000/tcp
+firewall-cmd --reload
+
+# prep the edge.ks file
+cat edge.ks.orig | \
+    sed "s/__HOST_IP__/$HOST_IP/g" | \
+    sed "s/__EDGE_IP__/$EDGE_IP/g" | \
+    sed "s/__EDGE_MASK__/$EDGE_MASK/g" | \
+    sed "s/__EDGE_GW__/$EDGE_GW/g" | \
+    sed "s/__EDGE_DNS__/$EDGE_DNS/g" > edge.ks
+
