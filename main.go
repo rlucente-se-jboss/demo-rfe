d8121fc219d094f9a42dda5f1af64e4447ff42c0
rlucente <rlucente@redhat.com>
2021-03-24 23:17:16 -0400

    log each file served

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func main() {
 	fs := http.FileServer(http.Dir("./"))
-	http.Handle("/", fs)
+	logFileServer := func(w http.ResponseWriter, req *http.Request) {
+		log.Println(req.URL.Path)
+		fs.ServeHTTP(w, req)
+	}
+
+	http.HandleFunc("/", logFileServer)
 
 	log.Println("Serving on :8000...")
 	err := http.ListenAndServe(":8000", nil)
