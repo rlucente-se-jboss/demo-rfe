3134c7127b8b79a18d6ffe96b641f7a158112d58
rlucente <rlucente@redhat.com>
2021-04-01 21:45:09 -0400

    track cumulative size of downloaded files

--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
+
+const MiB = 1024 * 1024
 
 func main() {
 	var count int
+	var total float64
 
 	fs := http.FileServer(http.Dir("./"))
 	logFileServer := func(w http.ResponseWriter, req *http.Request) {
 		count += 1
-		log.Printf("%5d %s\n", count, req.URL.Path)
+
+		fileInfo, err := os.Stat("." + req.URL.Path)
+		if err == nil {
+			total += float64(fileInfo.Size()) / MiB
+		}
+
+		log.Printf("%5d  %7.3f MiB  %s\n", count, total, req.URL.Path)
 		fs.ServeHTTP(w, req)
 	}
 
